f5f027279bd99c7125680d076074d0b3ba393cdb
Matthew Peters <matthew.peters.nyc@gmail.com>
2020-07-04 18:22:38 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,19 +1,32 @@
+src = [
+  ["Posh", "Scary", "Sporty"],
+  ["Paprika", "Fajita Mix", "Coriander"],
+  ["Parsley", "Sage", "Rosemary"]
+]
+
+
 def find_min_in_nested_arrays(src)
+  outer_results = []
   row_index = 0
-  min_values = []
   while row_index < src.count do
     element_index = 0
-    min_value = ""
-    sorted = ""
+    longest_string_element = ""
     while element_index < src[row_index].count do
-
-      src[row_index][element_index].sort = sorted
-
-
+   
+      # Array at row_index
+      # Element of the inner array at element_index
+      # If the length of the current element is greater than the length of longest_string_element
+      # Set longest_string_element to the current element
+      if src[row_index][element_index].length > longest_string_element.length
+        longest_string_element = src[row_index][element_index]
       end
       element_index += 1
     end
-    min_values << sorted.first
+   
+    # We have to check every element in each inner array, so after the loop finishes
+    # The current value of longest_string_element is pushed into outer_results
+    outer_results << longest_string_element
     row_index += 1
-    min_values
   end
+   
+  outer_results # => ["Sporty", "Fajita Mix", "Rosemary"]
